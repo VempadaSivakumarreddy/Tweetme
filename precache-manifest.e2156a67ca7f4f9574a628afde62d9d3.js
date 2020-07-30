629a248895d959e57f316b26d07678d02f80dfa7
VempadaSivakumarreddy <bima.in.dev1@gmail.com>
2020-07-30 12:03:54 +0530

    Updates

---
+++ precache-manifest.e2156a67ca7f4f9574a628afde62d9d3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b27d03f8aaa052427b5e363103bcefaf",
+    "url": "/Tweetme/index.html"
+  },
+  {
+    "revision": "361b163625db90fc40ea",
+    "url": "/Tweetme/static/css/main.b32aa182.chunk.css"
+  },
+  {
+    "revision": "7586cf2bad478b14ce85",
+    "url": "/Tweetme/static/js/2.f1c38ffd.chunk.js"
+  },
+  {
+    "revision": "c5757f2863d89aa08d26280ee8728033",
+    "url": "/Tweetme/static/js/2.f1c38ffd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "361b163625db90fc40ea",
+    "url": "/Tweetme/static/js/main.c9ca270c.chunk.js"
+  },
+  {
+    "revision": "88e36cabef061e738e56",
+    "url": "/Tweetme/static/js/runtime-main.97977446.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Tweetme/static/media/logo.5d5d9eef.svg"
+  }
+]);
